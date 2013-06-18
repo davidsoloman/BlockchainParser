9d896c4bd460dfbbb3d9989a046f50d7f91554d9
Raimo Radczewski <raimoradczewski@googlemail.com>
2013-06-18 10:44:30 +0200

    Removed old code

--- src/de/reemo/bitcoin/Transaction.java
+++
@@ -1,9 +0,0 @@
-package de.reemo.bitcoin;
-public class Transaction {
-	long version;
-	Number num_inputs;
-	TransactionInput[] inputs;
-	TransactionOutput[] outputs;
-	long lock_time;
-	Number num_outputs;
-}
